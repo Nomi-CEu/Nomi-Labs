871b973734d02f0ff87d5065d2a07cede5bd2ae6
IntegerLimit <103940576+IntegerLimit@users.noreply.github.com>
2023-09-14 16:46:49 +1000

    Add Microverse Casing as a SimpleOverlayRenderer

--- src/main/java/com/nomiceu/nomilabs/gregtech/LabsTextures.java
+++ src/main/java/com/nomiceu/nomilabs/gregtech/LabsTextures.java
@@ -1,14 +1,17 @@
 package com.nomiceu.nomilabs.gregtech;
 
 import gregtech.client.renderer.texture.cube.OrientedOverlayRenderer;
+import gregtech.client.renderer.texture.cube.SimpleOverlayRenderer;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 
 @SideOnly(Side.CLIENT)
 public class LabsTextures {
     public static OrientedOverlayRenderer GREENHOUSE_OVERLAY;
+    public static SimpleOverlayRenderer MICROVERSE_CASING;
 
     public static void preInit() {
         GREENHOUSE_OVERLAY = new OrientedOverlayRenderer("nomilabs:multiblock/greenhouse");
+        MICROVERSE_CASING = new SimpleOverlayRenderer("contenttweaker:microverse_casing");
     }
 }
