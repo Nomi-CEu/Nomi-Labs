ed3c9f51aaac7c2a39c0f4640fbc47bdb89a486b
IntegerLimit <103940576+IntegerLimit@users.noreply.github.com>
2023-12-20 20:59:04 +1100

    Give Up

--- src/main/java/com/nomiceu/nomilabs/core/LabsLateMixin.java
+++ src/main/java/com/nomiceu/nomilabs/core/LabsLateMixin.java
@@ -18,8 +18,7 @@ public class LabsLateMixin implements ILateMixinLoader {
     public static final Map<String, Boolean> modMixinsConfig = ImmutableMap.of(
             "draconicevolution", LabsConfig.modIntegration.draconicEvolutionIntegration.enableDraconicEvolutionIntegration,
             "nuclearcraft", LabsConfig.modIntegration.enableNuclearCraftIntegration,
-            "extrautils2", LabsConfig.modIntegration.enableExtraUtils2Integration,
-            "gregtech", true
+            "extrautils2", LabsConfig.modIntegration.enableExtraUtils2Integration
     );
 
     @Override
