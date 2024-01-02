c406dfad7986d99908836fa6d139fa88b8ab6729
IntegerLimit <103940576+IntegerLimit@users.noreply.github.com>
2024-01-02 13:35:48 +1100

    Small Cleanup

--- src/main/java/com/nomiceu/nomilabs/mixin/solarflux/INetworkSF.java
+++ src/main/java/com/nomiceu/nomilabs/mixin/solarflux/INetworkSF.java
@@ -1,17 +1,12 @@
 package com.nomiceu.nomilabs.mixin.solarflux;
 
-import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Mutable;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.gen.Accessor;
 import tk.zeitheron.solarflux.net.NetworkSF;
 
 @SuppressWarnings("unused")
 @Mixin(value = NetworkSF.class, remap = false)
 public interface INetworkSF {
-
-
     @Accessor
-    public static void setINSTANCE(NetworkSF instance) {}
+    static void setINSTANCE(NetworkSF instance) {}
 }
