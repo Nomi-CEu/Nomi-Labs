a1ee3d0890ee66cfc573e78200fe21af2a97bac5
Integer Limit <103940576+IntegerLimit@users.noreply.github.com>
2024-01-19 17:02:57 +0800

    Fix Crash on Server

--- src/main/java/com/nomiceu/nomilabs/gregtech/LabsTextures.java
+++ src/main/java/com/nomiceu/nomilabs/gregtech/LabsTextures.java
@@ -4,10 +4,7 @@ import com.nomiceu.nomilabs.util.LabsNames;
 import gregtech.api.gui.resources.TextureArea;
 import gregtech.client.renderer.texture.cube.OrientedOverlayRenderer;
 import gregtech.client.renderer.texture.cube.SimpleOverlayRenderer;
-import net.minecraftforge.fml.relauncher.Side;
-import net.minecraftforge.fml.relauncher.SideOnly;
 
-@SideOnly(Side.CLIENT)
 public class LabsTextures {
     /* Overlays (Machine) */
     public static OrientedOverlayRenderer GREENHOUSE_OVERLAY;
