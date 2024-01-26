5e56538345bb6ae0821105dba273e868b7fbfca4
Integer Limit <103940576+IntegerLimit@users.noreply.github.com>
2024-01-26 16:47:56 +0800

    Fix Recycling Recipes Missing
    Remove Recipe Map Mixin, Revert to Previous Behaviour

--- src/main/java/com/nomiceu/nomilabs/gregtech/RecipeMapAccessor.java
+++
@@ -1,8 +0,0 @@
-package com.nomiceu.nomilabs.gregtech;
-
-import gregtech.api.recipes.category.GTRecipeCategory;
-
-public interface RecipeMapAccessor {
-
-    void removeAllRecipesInCategory(GTRecipeCategory category);
-}
