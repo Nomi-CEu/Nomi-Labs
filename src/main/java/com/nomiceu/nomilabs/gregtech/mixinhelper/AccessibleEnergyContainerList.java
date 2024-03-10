e90a3cafe6c94cad88574a38c18deb44d17c87e7
Integer Limit <103940576+IntegerLimit@users.noreply.github.com>
2024-03-10 17:21:45 +1100

    Revert "Fix GT 4A Hatches Max Input Voltage Not Being Correct"
    This reverts commit 81da13f9f8c94d84bda2eeb6c2fc16d3016f0710.

--- src/main/java/com/nomiceu/nomilabs/gregtech/mixinhelper/AccessibleEnergyContainerList.java
+++
@@ -1,8 +0,0 @@
-package com.nomiceu.nomilabs.gregtech.mixinhelper;
-
-public interface AccessibleEnergyContainerList {
-    /**
-     * The total amperage of all the containers with the highest input voltage.
-     */
-    long getTotalHighestInputAmperage();
-}
