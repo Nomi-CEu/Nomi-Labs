06300fb5ca6873cad9f734d9bb98a3fe4a1b8c9e
Integer Limit <103940576+IntegerLimit@users.noreply.github.com>
2024-03-31 21:15:22 +1100

    Revert "Architecture Craft 3.108 Compat"
    This reverts commit b839785afb1888fa64173053e0bff225d0f0f847.
    
    Architecture Craft 3.108 has TOP issues.

--- src/main/java/com/nomiceu/nomilabs/mixin/architecturecraft/ShapeAccessor.java
+++ src/main/java/com/nomiceu/nomilabs/mixin/architecturecraft/ShapeAccessor.java
@@ -1,6 +1,6 @@
 package com.nomiceu.nomilabs.mixin.architecturecraft;
 
-import com.elytradev.architecture.common.shape.EnumShape;
+import com.elytradev.architecture.common.shape.Shape;
 import org.apache.commons.lang3.NotImplementedException;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.gen.Accessor;
@@ -10,10 +10,10 @@ import java.util.Map;
 /**
  * Allows accessing the protected static ID Map, which is used by Architecture Craft.
  */
-@Mixin(value = EnumShape.class, remap = false)
+@Mixin(value = Shape.class, remap = false)
 public interface ShapeAccessor {
     @Accessor(value = "idMap")
-    static Map<Integer, EnumShape> getIDMap() {
+    static Map<Integer, Shape> getIDMap() {
         throw new NotImplementedException("ShapeAccessorMixin Failed to Apply!");
     }
 }
