3d97f1127eae48a84c8dfbedce218b84494809e2
v3ect0rgames <164247766+v3ect0rgames@users.noreply.github.com>
2024-07-01 20:39:58 -0400

    Update EnchanterMixin.java

--- src/main/java/com/nomiceu/nomilabs/mixin/extrautils2/EnchanterMixin.java
+++ src/main/java/com/nomiceu/nomilabs/mixin/extrautils2/EnchanterMixin.java
@@ -1,16 +1,19 @@
-package com.yourmod.mixin;
+package com.nomiceu.nomilabs.mixin.extrautils2;
 
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
+import com.rwtema.extrautils2.api.machine.MachineSlotFluid;
+import com.rwtema.extrautils2.api.machine.MachineSlotItem;
 import com.rwtema.extrautils2.machine.MechEnchantmentRecipe;
-import net.minecraft.item.ItemStack;
 import net.minecraft.init.Items;
+import net.minecraft.item.ItemStack;
 import net.minecraftforge.fluids.FluidStack;
+import net.minecraftforge.oredict.OreDictionary;
+import org.apache.commons.lang3.tuple.Pair;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
-import org.apache.commons.lang3.tuple.Pair;
 
 import java.util.List;
 import java.util.Map;
@@ -23,13 +26,24 @@ public class MixinMechEnchantmentRecipe {
     private void modifyJEIInputItemExamples(CallbackInfoReturnable<List<Pair<Map<MachineSlotItem, List<ItemStack>>, Map<MachineSlotFluid, List<FluidStack>>>>> cir) {
         List<Pair<Map<MachineSlotItem, List<ItemStack>>, Map<MachineSlotFluid, List<FluidStack>>>> originalList = cir.getReturnValue();
 
+        // Get all items
+        List<ItemStack> matchingItems = GameRegistry.findRegistry(Item.class).getValues().stream()
+                .filter(item -> item.getRegistryName().getPath().startsWith("item.gt.tool")) // Change "iron_" to your desired prefix
+                .map(ItemStack::new)
+                .collect(Collectors.toList());
+       
+                
+        
+                
+
         // Modify the ImmutableMap here
         List<Pair<Map<MachineSlotItem, List<ItemStack>>, Map<MachineSlotFluid, List<FluidStack>>>> modifiedList = originalList.stream().map(pair -> {
-            Map<MachineSlotItem, List<ItemStack>> modifiedMap = new ImmutableMap.Builder<MachineSlotItem, List<ItemStack>>()
+            ImmutableMap<MachineSlotItem, List<ItemStack>> modifiedMap = new ImmutableMap.Builder<MachineSlotItem, List<ItemStack>>()
                     .putAll(pair.getLeft())
-                    .put(XUMachineEnchanter.INPUT, ImmutableList.of(
-                            Items.WOODEN_PICKAXE, Items.IRON_PICKAXE, Items.GOLDEN_PICKAXE, Items.DIAMOND_PICKAXE
-                    ).stream().map(ItemStack::new).collect(Collectors.toList()))
+                    .put(XUMachineEnchanter.INPUT, ImmutableList.<ItemStack>builder()
+                            .addAll(pair.getLeft().getOrDefault(XUMachineEnchanter.INPUT, ImmutableList.of()))
+                            .addAll(matchingItems) // Add all tools.
+                            .build())
                     .build();
 
             return Pair.of(modifiedMap, pair.getRight());
@@ -38,4 +52,3 @@ public class MixinMechEnchantmentRecipe {
         cir.setReturnValue(modifiedList);
     }
 }
-
