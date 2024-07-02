9a600083231fac312a90ec90ddab19e5feeecd79
Integer Limit <103940576+IntegerLimit@users.noreply.github.com>
2024-07-02 22:13:29 +1000

    Fix Dupe Bug with GT Tools and XU2 Enchanter
    Fixes https://github.com/Nomi-CEu/Nomi-CEu/issues/770
    
    Superseeds https://github.com/Nomi-CEu/Nomi-Labs/pull/22

---
+++ src/main/java/com/nomiceu/nomilabs/mixin/extrautils2/IMachineRecipeMixin.java
@@ -0,0 +1,43 @@
+package com.nomiceu.nomilabs.mixin.extrautils2;
+
+import java.util.Map;
+
+import net.minecraft.item.ItemStack;
+import net.minecraftforge.fluids.FluidStack;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import com.google.common.collect.ImmutableMap;
+import com.rwtema.extrautils2.api.machine.IMachineRecipe;
+import com.rwtema.extrautils2.api.machine.MachineSlotFluid;
+import com.rwtema.extrautils2.api.machine.MachineSlotItem;
+import com.rwtema.extrautils2.machine.TileMachine;
+
+/**
+ * Fixes XU2 Machine Recipes not consuming GT Tools Correctly, especially evident in the Enchanter.
+ * <p>
+ * This causes a dupe bug of GT Tools.
+ */
+@Mixin(value = IMachineRecipe.class, remap = false)
+public interface IMachineRecipeMixin {
+
+    /**
+     * Overrides (most) XU2 Machine Recipes to return a empty map instead of a map of collections of items, populated
+     * via {@link net.minecraftforge.common.ForgeHooks#getContainerItem(ItemStack)}.
+     * <p>
+     * This is because ItemGTTool overrides that method, in order to damage, instead of consume, GT Tools in Crafting
+     * Recipes.
+     * <p>
+     * See {@link TileMachine#consumeInputs()} for why this works. Essentially, if there is no container item map for
+     * the specified slot, an Empty ItemStack is used instead.
+     */
+    @Inject(method = "getContainerItems", at = @At("HEAD"), cancellable = true)
+    default void getEmptyStacks(Map<MachineSlotItem, ItemStack> inputItems,
+                                Map<MachineSlotFluid, FluidStack> inputFluids,
+                                CallbackInfoReturnable<Map<MachineSlotItem, ItemStack>> cir) {
+        cir.setReturnValue(ImmutableMap.of());
+    }
+}
