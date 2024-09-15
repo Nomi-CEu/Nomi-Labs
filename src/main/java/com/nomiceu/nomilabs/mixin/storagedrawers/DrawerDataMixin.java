37c494ca15e8ba283b987205165361baaf33d13f
Integer Limit <103940576+IntegerLimit@users.noreply.github.com>
2024-09-15 11:38:10 +1000

    Spotless

--- src/main/java/com/nomiceu/nomilabs/mixin/storagedrawers/DrawerDataMixin.java
+++ src/main/java/com/nomiceu/nomilabs/mixin/storagedrawers/DrawerDataMixin.java
@@ -1,13 +1,14 @@
 package com.nomiceu.nomilabs.mixin.storagedrawers;
 
-import com.jaquadro.minecraft.storagedrawers.api.storage.IDrawerAttributes;
-import com.jaquadro.minecraft.storagedrawers.api.storage.attribute.LockAttribute;
-import com.jaquadro.minecraft.storagedrawers.block.tile.tiledata.StandardDrawerGroup;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import com.jaquadro.minecraft.storagedrawers.api.storage.IDrawerAttributes;
+import com.jaquadro.minecraft.storagedrawers.api.storage.attribute.LockAttribute;
+import com.jaquadro.minecraft.storagedrawers.block.tile.tiledata.StandardDrawerGroup;
 
 @Mixin(value = StandardDrawerGroup.DrawerData.class, remap = false)
 public abstract class DrawerDataMixin {
