eadf7d1b98652fcd984f353b53a4f45cb82aeb2a
Integer Limit <103940576+IntegerLimit@users.noreply.github.com>
2024-10-01 23:04:49 +1000

    Fix Cleanroom Multi Crash

--- src/main/java/com/nomiceu/nomilabs/mixin/gregtech/MetaTileEntityCleanroomMixin.java
+++ src/main/java/com/nomiceu/nomilabs/mixin/gregtech/MetaTileEntityCleanroomMixin.java
@@ -1,3 +1,22 @@
 package com.nomiceu.nomilabs.mixin.gregtech;
 
-public class MetaTileEntityCleanroomMixin {}
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import gregtech.api.capability.IEnergyContainer;
+import gregtech.common.metatileentities.multi.electric.MetaTileEntityCleanroom;
+
+@Mixin(value = MetaTileEntityCleanroom.class, remap = false)
+public class MetaTileEntityCleanroomMixin {
+
+    @Shadow
+    private IEnergyContainer energyContainer;
+
+    @Inject(method = "drainEnergy", at = @At("HEAD"), cancellable = true)
+    private void checkNull(boolean simulate, CallbackInfoReturnable<Boolean> cir) {
+        if (energyContainer == null) cir.setReturnValue(false);
+    }
+}
