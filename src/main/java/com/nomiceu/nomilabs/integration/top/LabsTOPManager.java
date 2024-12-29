5f27b9d936f1f70050c71c8d9c40a78e5981c51c
Integer Limit <103940576+IntegerLimit@users.noreply.github.com>
2024-12-29 18:25:03 +1100

    Show Renamed AE2 Parts and Blocks in TOP

--- src/main/java/com/nomiceu/nomilabs/integration/top/LabsTOPManager.java
+++ src/main/java/com/nomiceu/nomilabs/integration/top/LabsTOPManager.java
@@ -4,6 +4,7 @@ import net.minecraft.block.Block;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.world.World;
+import net.minecraftforge.fml.common.Loader;
 
 import com.nomiceu.nomilabs.LabsValues;
 
@@ -13,12 +14,23 @@ import mcjty.theoneprobe.api.*;
 public class LabsTOPManager {
 
     public static int FLUID_NAME_ELEMENT;
+    public static int CUSTOM_NAME_ELEMENT;
 
     public static void register() {
         ITheOneProbe TOP = TheOneProbe.theOneProbeImp;
-        TOP.registerProvider(new TOPTooltipMessage());
+
+        // AE2 TOP Integration
+        if (Loader.isModLoaded(LabsValues.AE2_MODID))
+            TOP.registerBlockDisplayOverride(new AECustomNameOverride());
+
+        // GT TOP Integration
         TOP.registerProvider(new SteamMachineInfoProvider());
+
+        // Labs TOP Integration
+        TOP.registerProvider(new TOPTooltipMessage());
+
         FLUID_NAME_ELEMENT = TOP.registerElementFactory(LabsFluidNameElement::new);
+        CUSTOM_NAME_ELEMENT = TOP.registerElementFactory(CustomNameElement::new);
     }
 
     public static class TOPTooltipMessage implements IProbeInfoProvider {
