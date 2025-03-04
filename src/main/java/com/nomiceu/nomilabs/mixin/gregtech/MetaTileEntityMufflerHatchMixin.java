aa8b562a379017d6f2e48fdf091d7eb9c5b0920f
D-Alessian <alessian@alessian.be>
2025-03-04 23:47:00 +0100

    Change the muffler GUI to an info card stating mufflers don't generate ash anymore
    Signed-off-by: Alessian Dormal <alessian@alessian.be>

---
+++ src/main/java/com/nomiceu/nomilabs/mixin/gregtech/MetaTileEntityMufflerHatchMixin.java
@@ -0,0 +1,39 @@
+package com.nomiceu.nomilabs.mixin.gregtech;
+
+import gregtech.api.gui.widgets.ImageWidget;
+import gregtech.api.gui.widgets.SimpleTextWidget;
+import net.minecraft.util.ResourceLocation;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
+import gregtech.api.gui.ModularUI;
+import gregtech.api.gui.GuiTextures;
+import gregtech.api.gui.IUIHolder;
+import gregtech.common.metatileentities.multi.multiblockpart.MetaTileEntityMultiblockPart;
+import gregtech.common.metatileentities.multi.multiblockpart.MetaTileEntityMufflerHatch;
+import net.minecraft.entity.player.EntityPlayer;
+
+@Mixin(value = MetaTileEntityMufflerHatch.class, remap = false)
+public abstract class MetaTileEntityMufflerHatchMixin extends MetaTileEntityMultiblockPart {
+
+    // The mixin must provide a constructor matching the superclass.
+    protected MetaTileEntityMufflerHatchMixin(ResourceLocation metaTileEntityId, int tier) {
+        super(metaTileEntityId, tier);
+    }
+
+    /**
+     * @reason Overwrite createUI to display a custom message.
+     * @author D-Alessian
+     */
+    @Overwrite
+    protected ModularUI createUI(EntityPlayer entityPlayer) {
+        // Directly call getHolder() inherited from MetaTileEntityMultiblockPart
+        IUIHolder holder = this.getHolder();
+        ModularUI.Builder builder = ModularUI.builder(GuiTextures.BORDERED_BACKGROUND, 200, 100)
+                .widget(new ImageWidget(92, 16, 16, 16, GuiTextures.INFO_ICON))
+                .widget(
+                        new SimpleTextWidget(100, 60, "gregtech.gui.muffler.notify_change", 0x555555, () -> "")
+                                .setWidth(180)
+                );
+        return builder.build(holder, entityPlayer);
+    }
+}
