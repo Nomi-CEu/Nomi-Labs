038fc0777be6b63b5506b448ac5ce006694c9694
Alessian Dormal <alessian@alessian.be>
2025-03-05 23:48:43 +0100

    Change the way the muffler GUI is changed by using an inject instead of overwrite
    Signed-off-by: Alessian Dormal <alessian@alessian.be>

--- src/main/java/com/nomiceu/nomilabs/mixin/gregtech/MetaTileEntityMufflerHatchMixin.java
+++ src/main/java/com/nomiceu/nomilabs/mixin/gregtech/MetaTileEntityMufflerHatchMixin.java
@@ -1,18 +1,20 @@
 package com.nomiceu.nomilabs.mixin.gregtech;
 
+import com.nomiceu.nomilabs.config.LabsConfig;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.util.ResourceLocation;
 
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Overwrite;
 
 import gregtech.api.gui.GuiTextures;
-import gregtech.api.gui.IUIHolder;
 import gregtech.api.gui.ModularUI;
 import gregtech.api.gui.widgets.ImageWidget;
 import gregtech.api.gui.widgets.SimpleTextWidget;
 import gregtech.common.metatileentities.multi.multiblockpart.MetaTileEntityMufflerHatch;
 import gregtech.common.metatileentities.multi.multiblockpart.MetaTileEntityMultiblockPart;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(value = MetaTileEntityMufflerHatch.class, remap = false)
 public abstract class MetaTileEntityMufflerHatchMixin extends MetaTileEntityMultiblockPart {
@@ -22,19 +24,17 @@ public abstract class MetaTileEntityMufflerHatchMixin extends MetaTileEntityMult
         super(metaTileEntityId, tier);
     }
 
-    /**
-     * @reason Overwrite createUI to display a custom message.
-     * @author D-Alessian
-     */
-    @Overwrite
-    protected ModularUI createUI(EntityPlayer entityPlayer) {
-        // Directly call getHolder() inherited from MetaTileEntityMultiblockPart
-        IUIHolder holder = this.getHolder();
-        ModularUI.Builder builder = ModularUI.builder(GuiTextures.BORDERED_BACKGROUND, 200, 100)
-                .widget(new ImageWidget(92, 16, 16, 16, GuiTextures.INFO_ICON))
-                .widget(
-                        new SimpleTextWidget(100, 60, "gregtech.gui.muffler.notify_change", 0x555555, () -> "")
-                                .setWidth(180));
-        return builder.build(holder, entityPlayer);
+    @Inject(method = "createUI(Lnet/minecraft/entity/player/EntityPlayer;)Lgregtech/api/gui/ModularUI;",
+            at = @At("HEAD"), cancellable = true)
+    private void onCreateUI(EntityPlayer entityPlayer, CallbackInfoReturnable<ModularUI> cir) {
+        if (LabsConfig.modIntegration.enableDummyMufflers) {
+            ModularUI.Builder builder = ModularUI.builder(GuiTextures.BORDERED_BACKGROUND, 200, 100)
+                    .widget(new ImageWidget(92, 16, 16, 16, GuiTextures.INFO_ICON))
+                    .widget(
+                            new SimpleTextWidget(100, 60, "gregtech.gui.muffler.notify_change", 0x555555, () -> "")
+                                    .setWidth(180)
+                    );
+            cir.setReturnValue(builder.build(getHolder(), entityPlayer));
+        }
     }
 }
