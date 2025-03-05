0557bb0af795777ed594abeb8ef5715ff87892f2
Alessian Dormal <alessian@alessian.be>
2025-03-05 23:55:43 +0100

    Spotless
    Signed-off-by: Alessian Dormal <alessian@alessian.be>

--- src/main/java/com/nomiceu/nomilabs/mixin/gregtech/MetaTileEntityMufflerHatchMixin.java
+++ src/main/java/com/nomiceu/nomilabs/mixin/gregtech/MetaTileEntityMufflerHatchMixin.java
@@ -1,10 +1,14 @@
 package com.nomiceu.nomilabs.mixin.gregtech;
 
-import com.nomiceu.nomilabs.config.LabsConfig;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.util.ResourceLocation;
 
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import com.nomiceu.nomilabs.config.LabsConfig;
 
 import gregtech.api.gui.GuiTextures;
 import gregtech.api.gui.ModularUI;
@@ -12,9 +16,6 @@ import gregtech.api.gui.widgets.ImageWidget;
 import gregtech.api.gui.widgets.SimpleTextWidget;
 import gregtech.common.metatileentities.multi.multiblockpart.MetaTileEntityMufflerHatch;
 import gregtech.common.metatileentities.multi.multiblockpart.MetaTileEntityMultiblockPart;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(value = MetaTileEntityMufflerHatch.class, remap = false)
 public abstract class MetaTileEntityMufflerHatchMixin extends MetaTileEntityMultiblockPart {
@@ -25,15 +26,15 @@ public abstract class MetaTileEntityMufflerHatchMixin extends MetaTileEntityMult
     }
 
     @Inject(method = "createUI(Lnet/minecraft/entity/player/EntityPlayer;)Lgregtech/api/gui/ModularUI;",
-            at = @At("HEAD"), cancellable = true)
+            at = @At("HEAD"),
+            cancellable = true)
     private void onCreateUI(EntityPlayer entityPlayer, CallbackInfoReturnable<ModularUI> cir) {
         if (LabsConfig.modIntegration.enableDummyMufflers) {
             ModularUI.Builder builder = ModularUI.builder(GuiTextures.BORDERED_BACKGROUND, 200, 100)
                     .widget(new ImageWidget(92, 16, 16, 16, GuiTextures.INFO_ICON))
                     .widget(
                             new SimpleTextWidget(100, 60, "gregtech.gui.muffler.notify_change", 0x555555, () -> "")
-                                    .setWidth(180)
-                    );
+                                    .setWidth(180));
             cir.setReturnValue(builder.build(getHolder(), entityPlayer));
         }
     }
--- src/main/java/com/nomiceu/nomilabs/mixin/gregtech/MultiblockRecipeLogicMixin.java
+++ src/main/java/com/nomiceu/nomilabs/mixin/gregtech/MultiblockRecipeLogicMixin.java
@@ -1,14 +1,13 @@
 package com.nomiceu.nomilabs.mixin.gregtech;
 
-import com.nomiceu.nomilabs.config.LabsConfig;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
+import com.nomiceu.nomilabs.config.LabsConfig;
 import com.nomiceu.nomilabs.gregtech.mixinhelper.IRefreshBeforeConsumption;
 
 import gregtech.api.capability.impl.AbstractRecipeLogic;
@@ -30,7 +29,8 @@ public abstract class MultiblockRecipeLogicMixin extends AbstractRecipeLogic {
         super(tileEntity, recipeMap);
     }
 
-    @Shadow protected abstract void performMufflerOperations();
+    @Shadow
+    protected abstract void performMufflerOperations();
 
     @Inject(method = "prepareRecipeDistinct", at = @At("HEAD"))
     private void refresh(Recipe recipe, CallbackInfoReturnable<Boolean> cir) {
