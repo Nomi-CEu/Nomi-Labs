940041533d5550895dc4a007cb9f68e6b8d94c3a
Integer Limit <103940576+IntegerLimit@users.noreply.github.com>
2025-06-18 22:16:58 +1000

    Cleanup Code in SlotRestrictedInputMixin

--- src/main/java/com/nomiceu/nomilabs/mixin/ae2/SlotRestrictedInputMixin.java
+++ src/main/java/com/nomiceu/nomilabs/mixin/ae2/SlotRestrictedInputMixin.java
@@ -1,7 +1,5 @@
 package com.nomiceu.nomilabs.mixin.ae2;
 
-import java.util.Optional;
-
 import net.minecraft.item.ItemStack;
 
 import org.spongepowered.asm.mixin.Final;
@@ -10,8 +8,6 @@ import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
-
-import com.nomiceu.nomilabs.util.ItemMeta;
 
 import appeng.api.AEApi;
 import appeng.container.slot.SlotRestrictedInput;
@@ -36,12 +32,7 @@ public class SlotRestrictedInputMixin {
             require = 1)
     private void properlyCheckQuantumLink(ItemStack i, CallbackInfoReturnable<Boolean> cir) {
         if (which == SlotRestrictedInput.PlacableItemType.CARD_QUANTUM) {
-            Optional<ItemStack> quant = AEApi.instance().definitions().materials().cardQuantumLink().maybeStack(1);
-            if (!quant.isPresent()) {
-                cir.setReturnValue(false);
-                return;
-            }
-            cir.setReturnValue(ItemMeta.compare(quant.get(), i));
+            cir.setReturnValue(AEApi.instance().definitions().materials().cardQuantumLink().isSameAs(i));
         }
     }
 }
