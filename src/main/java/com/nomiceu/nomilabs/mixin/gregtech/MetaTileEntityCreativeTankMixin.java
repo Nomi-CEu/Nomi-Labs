46fa9095f12e5260813fa3087bdb61b45a50414f
Integer Limit <103940576+IntegerLimit@users.noreply.github.com>
2025-11-01 11:56:38 +1100

    Creative Tank Tooltip

--- src/main/java/com/nomiceu/nomilabs/mixin/gregtech/MetaTileEntityCreativeTankMixin.java
+++ src/main/java/com/nomiceu/nomilabs/mixin/gregtech/MetaTileEntityCreativeTankMixin.java
@@ -1,6 +1,11 @@
 package com.nomiceu.nomilabs.mixin.gregtech;
 
+import java.util.List;
+
+import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.util.ResourceLocation;
+import net.minecraft.world.World;
 import net.minecraftforge.fluids.FluidStack;
 import net.minecraftforge.fluids.capability.IFluidTankProperties;
 
@@ -13,12 +18,14 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import com.nomiceu.nomilabs.gregtech.mixinhelper.AccessibleCreativeTank;
 import com.nomiceu.nomilabs.integration.top.CustomFluidTankProvider;
+import com.nomiceu.nomilabs.util.LabsTranslate;
 
 import gregtech.common.metatileentities.storage.MetaTileEntityCreativeTank;
 import gregtech.common.metatileentities.storage.MetaTileEntityQuantumTank;
 
 /**
  * Makes the default mbPerTick be Int.MAX_VALUE, provides no fluid info to TOP to allow for custom handler.
+ * Adds the configured fluid to tooltip.
  * TODO: Maybe change in GT 2.9 when Creative Tanks have proper internal tank?
  */
 @Mixin(value = MetaTileEntityCreativeTank.class, remap = false)
@@ -41,6 +48,18 @@ public class MetaTileEntityCreativeTankMixin extends MetaTileEntityQuantumTank
     @Inject(method = "<init>", at = @At("RETURN"))
     private void setDefaultMbPerTick(ResourceLocation metaTileEntityId, CallbackInfo ci) {
         mBPerCycle = Integer.MAX_VALUE;
+    }
+
+    @Inject(method = "addInformation", at = @At("TAIL"))
+    private void addFluidInfo(ItemStack stack, @Nullable World player, List<String> tooltip, boolean advanced,
+                              CallbackInfo ci) {
+        NBTTagCompound tag = stack.getTagCompound();
+        if (tag == null || !tag.hasKey("Fluid")) return;
+
+        FluidStack fluid = FluidStack.loadFluidStackFromNBT(tag.getCompoundTag("Fluid"));
+        if (fluid == null) return;
+
+        tooltip.add(LabsTranslate.translate("tooltip.nomilabs.creative_tank.configured", fluid.getLocalizedName()));
     }
 
     @Override
