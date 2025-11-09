7103d59a95df09f434b117a2c38483dd42857e56
Integer Limit <103940576+IntegerLimit@users.noreply.github.com>
2025-11-09 16:30:22 +1100

    Fix Crash with New Mixinbooter Versions

--- src/main/java/com/nomiceu/nomilabs/mixin/extrautils2/MechEnchantmentRecipeMixin.java
+++ src/main/java/com/nomiceu/nomilabs/mixin/extrautils2/MechEnchantmentRecipeMixin.java
@@ -7,26 +7,24 @@ import net.minecraftforge.common.ForgeHooks;
 import net.minecraftforge.fluids.FluidStack;
 
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import com.google.common.collect.ImmutableMap;
 import com.rwtema.extrautils2.api.machine.IMachineRecipe;
 import com.rwtema.extrautils2.api.machine.MachineSlotFluid;
 import com.rwtema.extrautils2.api.machine.MachineSlotItem;
+import com.rwtema.extrautils2.machine.MechEnchantmentRecipe;
 import com.rwtema.extrautils2.machine.TileMachine;
 
 /**
- * Fixes XU2 Machine Recipes not consuming GT Tools Correctly, especially evident in the Enchanter.
+ * Fixes Enchantment Recipes not consuming GT Tools Correctly, such as in the Enchanter.
  * <p>
  * This causes a dupe bug of GT Tools.
  */
-@Mixin(value = IMachineRecipe.class, remap = false)
-public interface IMachineRecipeMixin {
+@Mixin(value = MechEnchantmentRecipe.class, remap = false)
+public abstract class MechEnchantmentRecipeMixin implements IMachineRecipe {
 
     /**
-     * Overrides (most) XU2 Machine Recipes to return a empty map instead of a map of collections of items, populated
+     * Overrides the Enchanting Recipes to return a empty map instead of a map of collections of items, populated
      * via {@link ForgeHooks#getContainerItem(ItemStack)}.
      * <p>
      * This is because ItemGTTool overrides that method, in order to damage, instead of consume, GT Tools in Crafting
@@ -35,11 +33,9 @@ public interface IMachineRecipeMixin {
      * See {@link TileMachine#consumeInputs()} for why this works. Essentially, if there is no container item map for
      * the specified slot, an Empty ItemStack is used instead.
      */
-    // Interface cannot have private methods (Java 8)
-    @Inject(method = "getContainerItems", at = @At("HEAD"), cancellable = true)
-    default void getEmptyStacks(Map<MachineSlotItem, ItemStack> inputItems,
-                                Map<MachineSlotFluid, FluidStack> inputFluids,
-                                CallbackInfoReturnable<Map<MachineSlotItem, ItemStack>> cir) {
-        cir.setReturnValue(ImmutableMap.of());
+    @Override
+    public Map<MachineSlotItem, ItemStack> getContainerItems(Map<MachineSlotItem, ItemStack> inputItems,
+                                                             Map<MachineSlotFluid, FluidStack> inputFluids) {
+        return ImmutableMap.of();
     }
 }
