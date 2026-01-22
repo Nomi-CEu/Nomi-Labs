c25aa25fadc9c3a077f69fd92eab65a03dea4a72
Integer Limit <103940576+IntegerLimit@users.noreply.github.com>
2026-01-22 19:55:42 +0800

    Fix Auto Output Button Rendering for GT Console

---
+++ src/main/java/com/nomiceu/nomilabs/mixin/gregtech/MachineConsoleWidgetMixin.java
@@ -0,0 +1,79 @@
+package com.nomiceu.nomilabs.mixin.gregtech;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import gregtech.api.gui.GuiTextures;
+import gregtech.api.gui.Widget;
+import gregtech.api.gui.resources.TextureArea;
+import gregtech.api.gui.widgets.ToggleButtonWidget;
+import gregtech.api.gui.widgets.WidgetGroup;
+import gregtech.api.util.Position;
+import gregtech.common.terminal.app.console.MachineConsoleWidget;
+
+/**
+ * Fixes the rendering of toggle auto-output buttons in the GT Console App.
+ */
+@Mixin(value = MachineConsoleWidget.class, remap = false)
+public class MachineConsoleWidgetMixin extends WidgetGroup {
+
+    @Unique
+    private static final String LABS$OUTPUT_HOVER = "terminal.console.auto_output";
+
+    @Inject(method = "initWidgets", at = @At("TAIL"))
+    private void fixAutoOutputWidgets(CallbackInfo ci) {
+        // Re-add item/fluid output after, to prevent CME
+        int itemIdx = -1;
+        int fluidIdx = -1;
+
+        for (int i = 0; i < widgets.size(); i++) {
+            if (!(widgets.get(i) instanceof CircleButtonWidgetAccessor circle))
+                continue;
+
+            // Does its hover text match item/fluid output buttons?
+            String[] hoverText = circle.labs$getHoverText();
+            if (hoverText == null || !(hoverText.length == 1 && LABS$OUTPUT_HOVER.equals(hoverText[0])))
+                continue;
+
+            // Check if its an item
+            if (((TextureArea) circle.labs$getIcon()).imageLocation == GuiTextures.BUTTON_ITEM_OUTPUT.imageLocation) {
+                itemIdx = i;
+            } else {
+                // It must be a fluid
+                fluidIdx = i;
+            }
+        }
+
+        // Perform replacements
+        labs$replaceOutputWidget(itemIdx, "gregtech.gui.item_auto_output.tooltip", GuiTextures.BUTTON_ITEM_OUTPUT);
+        labs$replaceOutputWidget(fluidIdx, "gregtech.gui.fluid_auto_output.tooltip", GuiTextures.BUTTON_FLUID_OUTPUT);
+    }
+
+    @Unique
+    private void labs$replaceOutputWidget(int idx, String tooltip, TextureArea texture) {
+        if (idx == -1) return;
+
+        Widget widget = widgets.get(idx);
+        widgets.remove(idx);
+
+        var rect = (RectButtonWidgetAccessor) widget;
+
+        // Click data is unused by the original widget handling; simply put in null
+        addWidget(idx,
+                new ToggleButtonWidget(widget.getSelfPosition().x, widget.getSelfPosition().y, widget.getSize().width,
+                        widget.getSize().height,
+                        texture, () -> rect.labs$getSupplier().get(),
+                        (pressed) -> rect.labs$getOnPressed().accept(null, pressed))
+                                .setTooltipText(tooltip)
+                                .shouldUseBaseBackground());
+
+        // Perform discard widget actions
+        widget.setUiAccess(null);
+        widget.setGui(null);
+        widget.setSizes(null);
+        widget.setParentPosition(Position.ORIGIN);
+    }
+}
